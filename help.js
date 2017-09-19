d703bbd0c7cd5363050fe2b62337114b4d5d3c6c
Misty Malkasian <andrewandmisty@gmail.com>
2017-09-19 13:54:18 -0500

    first commit

---
+++ help.js
@@ -0,0 +1,12 @@
+<script>var troll = prompt("You're walking through the forest, minding your own business, and you run into a troll! Do you FIGHT him, PAY him, or RUN?").toUpperCase();
+
+switch(troll) {
+  case 'FIGHT':
+    var strong = prompt("How courageous! Are you strong (YES or NO)?").toUpperCase();
+    var smart = prompt("Are you smart?").toUpperCase();
+    if(strong === 'YES' || smart === 'YES') {
+      console.log("You only need one of the two! You beat the troll--nice work!");
+    } else {
+      console.log("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a troll. You lose!");
+    }
+    </script>
