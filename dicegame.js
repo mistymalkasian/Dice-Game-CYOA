6df660e24d903ff8f6e7d273d91f1659d5e0d0f8
Misty Malkasian <andrewandmisty@gmail.com>
2017-09-21 12:52:03 -0500

    its almost fully functional, have a tiny thing or two to fix

--- dicegame.js
+++ dicegame.js
@@ -1,38 +1,64 @@
 
 
-function roll(dice, sides) {
+function roll(dice, sides){
 	let results = [];
 	
 	for (i = 0; i < dice; i++) {
 		results.push(Math.floor((Math.random() * sides) + 1));
 	
 	}
-	console.log("You have rolled " +results+"!");
+	console.log("You have rolled "+results+"!");
 }
 
 
-function determineWinOrLose(totaledScore){
-		let roll4 = (roll(1,4))
-		let roll6 = (roll(1,6))
-		let roll8 = (roll(1,8))
-		let roll10 = (roll(1,10))
-		let roll12 = (roll(1,12))
-		let roll20 = (roll(1,20))
+function determineOutcome(totaledScore){
+
 			 function addedRollTotals(roll4, roll6, roll8, roll10, roll12, roll20){
-			addedRollTotals=roll4+roll6+roll8+roll10+roll12+roll20
+			 	let roll4 = roll(1,4)
+				let roll6 = roll(1,6)
+				let roll8 = roll(1,8)  
+				let roll10 = roll(1,10) 
+				let roll12 = roll(1,12) 
+				let roll20= roll(1,20) 
+
+                roll4+roll6+roll8+roll10+roll12+roll20
+
+         return addedRollTotals(roll4), (roll6), (roll8), (roll10), (roll12), (roll20)       	
+		}
+		
+
+	if (addedRollTotals == 42)	
+		alert("YOU HAVE BECOME A MAGICAL UNICORN!");
+	else if (addedRollTotals > 42)
+		alert("YOU HAVE BECOME A HIDEOUS TROLLFACE MEME!");
+	else if (addedRollTotals < 39)
+		alert("You lose! Try again!!!");
+	else
+		alert("glorb!") 
+
+}	
+
+function addedRollTotals(roll4, roll6, roll8, roll10, roll12, roll20){
+			 	let roll4 = roll(1,4)
+				let roll6 = roll(1,6)
+				let roll8 = roll(1,8)  
+				let roll10 = roll(1,10) 
+				let roll12 = roll(1,12) 
+				let roll20= roll(1,20) 
+
+                roll4+roll6+roll8+roll10+roll12+roll20
+
 		}
 
-	if  (addedRollTotals < 42)	
 
-		alert("Sorry, you lost! Play again!");
-	else
-		alert("You won!!!");
-}			
+determineOutcome(addedRollTotals)
+
+
+
+
+
+
 	
-
-
-determineWinOrLose()
-
 
 
 
