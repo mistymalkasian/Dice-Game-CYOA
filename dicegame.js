54fabf8723f27e4747b47be5cb645226568f1d40
Misty Malkasian <andrewandmisty@gmail.com>
2017-09-21 15:07:59 -0500

    one more hiccup then it should work. Thanks to Mike lol

--- dicegame.js
+++ dicegame.js
@@ -1,64 +1,38 @@
 
 
-function roll(dice, sides){
-	let results = [];
-	
-	for (i = 0; i < dice; i++) {
-		results.push(Math.floor((Math.random() * sides) + 1));
-	
-	}
+function roll(sides){
+	let results = (Math.floor((Math.random() * sides) + 1));
 	console.log("You have rolled "+results+"!");
+	return results;
+
 }
 
 
-function determineOutcome(totaledScore){
+function addRollTotals(){
 
-			 function addedRollTotals(roll4, roll6, roll8, roll10, roll12, roll20){
-			 	let roll4 = roll(1,4)
-				let roll6 = roll(1,6)
-				let roll8 = roll(1,8)  
-				let roll10 = roll(1,10) 
-				let roll12 = roll(1,12) 
-				let roll20= roll(1,20) 
+	let roll4 = roll(4)
+	let roll6 = roll(6)
+	let roll8 = roll(8)
+	let roll10 = roll(10)
+	let roll12 = roll(12) 
+	let roll20 = roll(20)
 
-                roll4+roll6+roll8+roll10+roll12+roll20
+    let totaledScore = (roll4)+(roll6)+(roll8)+(roll10)+(roll12)+(roll20)
 
-         return addedRollTotals(roll4), (roll6), (roll8), (roll10), (roll12), (roll20)       	
-		}
-		
+            			
 
-	if (addedRollTotals == 42)	
-		alert("YOU HAVE BECOME A MAGICAL UNICORN!");
-	else if (addedRollTotals > 42)
-		alert("YOU HAVE BECOME A HIDEOUS TROLLFACE MEME!");
-	else if (addedRollTotals < 39)
-		alert("You lose! Try again!!!");
-	else
-		alert("glorb!") 
+	if (totaledScore == 42)
+		alert("Your total score is..."+totaledScore+". YOU HAVE BECOME A MAGICAL UNICORN!");
+	else if (totaledScore > 42)
+		alert("Your total score is..."+totaledScore+". YOU HAVE SOMEHOW MORPHED INTO A HIDEOUS TROLLFACE!");
+	else if (totaledScore == 39 || totaledScore == 40 || totaledScore == 41 )
+		alert("Your total score is..."+totaledScore+". What's this? It seems you've won! Congratulations!");
+	else if (totaledScore <= 38)
+		alert("Your total score is..."+totaledScore+". You have unfortunately failed. Try again!!!"); 
 
+	return totaledScore;  
 }	
 
-function addedRollTotals(roll4, roll6, roll8, roll10, roll12, roll20){
-			 	let roll4 = roll(1,4)
-				let roll6 = roll(1,6)
-				let roll8 = roll(1,8)  
-				let roll10 = roll(1,10) 
-				let roll12 = roll(1,12) 
-				let roll20= roll(1,20) 
-
-                roll4+roll6+roll8+roll10+roll12+roll20
-
-		}
-
-
-determineOutcome(addedRollTotals)
-
-
-
-
-
-
-	
 
 
 
