9c3e4d15ce8f5d90fa21ff13894b0f603995fa77
Misty Malkasian <andrewandmisty@gmail.com>
2017-09-21 09:50:47 -0500

    close to the home stretch. functions almost work.

--- dicegame.js
+++ dicegame.js
@@ -10,13 +10,24 @@ function roll(dice, sides) {
 	console.log("You have rolled " +results+"!");
 }
 
-function hideButton() {
-    let div = document.getElementyById();
-    div.style.display = 'none';
-}
 
 
+function determineWinOrLose(){
+		let roll4 = (roll(1,4))
+		let roll6 = (roll(1,6))
+		let roll8 = (roll(1,8))
+		let roll10 = (roll(1,10))
+		let roll12 = (roll(1,12))
+		let roll20 = (roll(1,20))
+		let addRollTotals= roll4+roll6+roll8+roll10+roll12+roll20
 
+	if  (addRollTotals > 42)	
+
+		console.log ("Sorry, you lost! Play again!");
+	else
+		console.log ("You won!!!");
+}			
+	
 
 
 
