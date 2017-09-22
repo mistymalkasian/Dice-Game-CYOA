9c64dbf62b008902b99c22621b6e5d734ed75397
Misty Malkasian <andrewandmisty@gmail.com>
2017-09-22 08:06:43 -0500

    game is perfect. images added too

--- dicegame.js
+++ dicegame.js
@@ -15,11 +15,11 @@ function endgameMessage(){
 			let totalScore = score[0]+score[1]+score[2]+score[3]+score[4]+score[5];
 
 	if (totalScore == 42){
-		alert("Your total score is..."+totalScore+". HOW AMAZING!!! YOU HAVE BECOME A MAGICAL UNICORN!");
+		alert("Your total score is..."+totalScore+". HOW AMAZING!!! Do you realize how rare this is?! SURPRISE: YOU HAVE BECOME A MAGICAL UNICORN!");
 	}
 	
 	else if (totalScore > 42){
-		alert("Your total score is..."+totalScore+". You apparently stink at this game.");
+		alert("Your total score is..."+totalScore+". You open a fortune cookie and it reads thus: 'YOU WILL WAKE UP TOMORROW AND NOT RECOGNIZE YOURSELF IN THE MIRROR BECAUSE YOUR FACE WILL HAVE MORPHED INTO THE LIKENESS OF THE TROLLFACE MEME!' Ooh. Better try again and end on a high note... you do NOT want it to end this way.");
 	}
 	
 	else if (totalScore == 39 || totalScore == 40 || totalScore == 41 ){
@@ -27,7 +27,7 @@ function endgameMessage(){
 	}
 	
 	else if (totalScore <= 38){
-		alert("Your total score is..."+totalScore+". You have unfortunately failed. Try again!!!");
+		alert("Your total score is..."+totalScore+". You, unfortunate soul, have failed. That's ok though, just try again!");
 	}
 		
 	} 
