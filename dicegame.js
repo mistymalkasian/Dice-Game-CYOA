2221484fba17b4d9c3974b45c20636009d8c6654
Misty Malkasian <andrewandmisty@gmail.com>
2017-09-22 07:20:40 -0500

    completed game is simple but works to perfection

--- dicegame.js
+++ dicegame.js
@@ -1,37 +1,37 @@
 
 
+
+let score = [];
+
+
 function roll(sides){
 	let results = (Math.floor((Math.random() * sides) + 1));
 	console.log("You have rolled "+results+"!");
-	return results;
-
+    score.push(results);
 }
 
+function endgameMessage(){
 
-function addRollTotals(){
+			let totalScore = score[0]+score[1]+score[2]+score[3]+score[4]+score[5];
 
-	let roll4 = roll(4)
-	let roll6 = roll(6)
-	let roll8 = roll(8)
-	let roll10 = roll(10)
-	let roll12 = roll(12) 
-	let roll20 = roll(20)
-
-    let totaledScore = (roll4)+(roll6)+(roll8)+(roll10)+(roll12)+(roll20)
-
-            			
-
-	if (totaledScore == 42)
-		alert("Your total score is..."+totaledScore+". YOU HAVE BECOME A MAGICAL UNICORN!");
-	else if (totaledScore > 42)
-		alert("Your total score is..."+totaledScore+". YOU HAVE SOMEHOW MORPHED INTO A HIDEOUS TROLLFACE!");
-	else if (totaledScore == 39 || totaledScore == 40 || totaledScore == 41 )
-		alert("Your total score is..."+totaledScore+". What's this? It seems you've won! Congratulations!");
-	else if (totaledScore <= 38)
-		alert("Your total score is..."+totaledScore+". You have unfortunately failed. Try again!!!"); 
-
-	return totaledScore;  
-}	
+	if (totalScore == 42){
+		alert("Your total score is..."+totalScore+". HOW AMAZING!!! YOU HAVE BECOME A MAGICAL UNICORN!");
+	}
+	
+	else if (totalScore > 42){
+		alert("Your total score is..."+totalScore+". You apparently stink at this game.");
+	}
+	
+	else if (totalScore == 39 || totalScore == 40 || totalScore == 41 ){
+		alert("Your total score is..."+totalScore+". What's this? It seems you've won! Congratulations!");
+	}
+	
+	else if (totalScore <= 38){
+		alert("Your total score is..."+totalScore+". You have unfortunately failed. Try again!!!");
+	}
+		
+	} 
+	
 
 
 
